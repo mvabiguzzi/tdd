e9d3197116336f505f4110da088d41a97a003ff1
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-18 20:33:13 -0300

    Alteração na classe "TestaMaiorMenor" para simular o seu funcionamento com os produtos adicionados ao carrinho em ordem decrescente de preço

--- src/main/java/br/com/mvabiguzzi/tdd/lojaVirtual/TestaMaiorMenor.java
+++ src/main/java/br/com/mvabiguzzi/tdd/lojaVirtual/TestaMaiorMenor.java
@@ -4,8 +4,8 @@ public class TestaMaiorMenor {
 	public static void main(String[] args) {
 		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
 		
-		carrinho.adiciona(new Produto("Liquidificador", 250.0));
 		carrinho.adiciona(new Produto("Geladeira", 450.0));
+		carrinho.adiciona(new Produto("Liquidificador", 250.0));
 		carrinho.adiciona(new Produto("Jogo de pratos", 70.0));
 		
 		MaiorMenor algoritimo = new MaiorMenor();
