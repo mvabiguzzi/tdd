3aa876b67479afb7b6d9db624d467d8605b8dc54
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-19 16:44:08 -0300

    Criação do teste para validar também a conversão do numero romano V para arabico

--- src/test/java/br/com/mvabiguzzi/tdd/numerosRomanos/ConversorDeNumerosRomanosTest.java
+++ src/test/java/br/com/mvabiguzzi/tdd/numerosRomanos/ConversorDeNumerosRomanosTest.java
@@ -15,4 +15,12 @@ public class ConversorDeNumerosRomanosTest {
 		assertEquals(1, numero);
 	}
 	
+	@Test
+	public void deveEntenterSimbuloV() {
+		ConversorDeNumerosRomanos romano = new ConversorDeNumerosRomanos();
+		
+		int numero = romano.converte("V");
+		
+		assertEquals(5, numero);
+	}
 }
