977ff02f8a7f8c1c175d67b3c7433592ff0929aa
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-19 17:49:26 -0300

    Refatoração na funcionalidade para converção de números romanos em arábicos para que fosse utilizada uma solução mais eficiente para os números romanos com dois simbolos (como II)

--- src/main/java/br/com/mvabiguzzi/tdd/numerosRomanos/ConversorDeNumerosRomanos.java
+++ src/main/java/br/com/mvabiguzzi/tdd/numerosRomanos/ConversorDeNumerosRomanos.java
@@ -4,19 +4,24 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ConversorDeNumerosRomanos {
-	private static Map<String, Integer> tabela = new HashMap<String, Integer>() { {
-		put("I", 1);
-		put("II", 2);
-		put("V", 5);
-		put("X", 10);
-		put("L", 50);
-		put("C", 100);
-		put("D", 500);
-		put("M", 1000);
+	private static Map<Character, Integer> tabela = new HashMap<Character, Integer>() { {
+		put('I', 1);
+		put('V', 5);
+		put('X', 10);
+		put('L', 50);
+		put('C', 100);
+		put('D', 500);
+		put('M', 1000);
 	} };
 	
 	public int converte(String numeroEmRomano) {
-		return tabela.get(numeroEmRomano);
+		int acumulador = 0;
+		
+		for(int i = 0; i < numeroEmRomano.length(); i++) {
+			acumulador += tabela.get(numeroEmRomano.charAt(i));
+		}
+		
+		return acumulador;
 	}
 	
 }
