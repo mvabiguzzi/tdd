19a4e25c55209c7dd102af66595366c859f8a3f0
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-20 14:41:26 -0300

    Refatoração na calculadora de salarios para simplificar a solução

--- src/main/java/br/com/mvabiguzzi/tdd/calculoSalario/CalculadoraSalario.java
+++ src/main/java/br/com/mvabiguzzi/tdd/calculoSalario/CalculadoraSalario.java
@@ -3,15 +3,11 @@ package br.com.mvabiguzzi.tdd.calculoSalario;
 public class CalculadoraSalario {
 	
 	public double calculaSalario(Funcionario funcionario) {
-		if(funcionario.getCargo().equals(Cargo.DESENVOLVEDOR)) {
-			if(funcionario.getSalario() > 3000) {
-				return funcionario.getSalario()*0.8;
-			}
-			
-			return funcionario.getSalario()*0.9;
+		if(funcionario.getSalario()>funcionario.getCargo().limite) {
+			return funcionario.getSalario()*funcionario.getCargo().descontoAcimaLimite;
 		}
 		
-		return funcionario.getSalario()*0.85;
+		return funcionario.getSalario()*funcionario.getCargo().descontoAbaixoLimite;
 	}
 	
 }
--- src/main/java/br/com/mvabiguzzi/tdd/calculoSalario/Cargo.java
+++ src/main/java/br/com/mvabiguzzi/tdd/calculoSalario/Cargo.java
@@ -1,7 +1,17 @@
 package br.com.mvabiguzzi.tdd.calculoSalario;
 
 public enum Cargo {
-	DESENVOLVEDOR,
-	DBA,
-	TESTADOR
+	DESENVOLVEDOR(3000.0,0.9,0.8),
+	DBA(2500.0,0.85,0.75),
+	TESTADOR(2500.0,0.85,0.75);
+	
+	public double limite;
+	public double descontoAbaixoLimite;
+	public double descontoAcimaLimite;
+	
+	Cargo(double limite, double descontoAbaixoLimite, double descontoAcimaLimite) {
+		this.limite = limite;
+		this.descontoAbaixoLimite = descontoAbaixoLimite;
+		this.descontoAcimaLimite = descontoAcimaLimite;
+	}
 }
