063cc438fa75a4a56527f4a37f992ad7a5cc70f5
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-23 18:14:46 -0300

    Implementação do caso de teste para validar a logica que busca o maior preço dentro de um carrinho de compras com muitos itens

--- src/test/java/br/com/mvabiguzzi/tdd/lojaVirtual/MaiorPrecoTest.java
+++ src/test/java/br/com/mvabiguzzi/tdd/lojaVirtual/MaiorPrecoTest.java
@@ -31,4 +31,23 @@ public class MaiorPrecoTest {
 		
 		assertEquals(900.0, valor, 0.0001);
 	}
+	
+	@Test
+	public void deveRetornarMaiorValorSeCarrinhoContemMuitosElementos() {
+		Produto geladeira = new Produto("Geladeira", 900.0);
+		Produto fogao = new Produto("Fogao", 1500.0);
+		Produto maquinaDeLavar = new Produto("Maquina de Lavar", 750.0);
+		
+		CarrinhoDeCompras carrinho = new CarrinhoDeCompras();
+		
+		carrinho.adiciona(new Item(geladeira, 1));
+		carrinho.adiciona(new Item(fogao, 1));
+		carrinho.adiciona(new Item(maquinaDeLavar, 1));
+		
+		MaiorPreco algoritimo = new MaiorPreco();
+		
+		double valor = algoritimo.encontra(carrinho);
+		
+		assertEquals(1500.0, valor, 0.0001);
+	}
 }
