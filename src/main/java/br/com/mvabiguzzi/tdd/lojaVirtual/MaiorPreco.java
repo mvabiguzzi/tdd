e7645b26d6e5dbe5d3cfc3f39dc7855ac637c60e
Marcos Vinícius Almeida Biguzzi <contato@mvabiguzzi.com.br>
2015-03-23 18:18:08 -0300

    Refatoração na funcionalidade para buscar o maior preço dentre os itens do carrinho de compras para simplificar a solução

--- src/main/java/br/com/mvabiguzzi/tdd/lojaVirtual/MaiorPreco.java
+++ src/main/java/br/com/mvabiguzzi/tdd/lojaVirtual/MaiorPreco.java
@@ -3,9 +3,7 @@ package br.com.mvabiguzzi.tdd.lojaVirtual;
 public class MaiorPreco {
 	
 	public double encontra(CarrinhoDeCompras carrinho) {
-		if(carrinho.getItens().size() == 0) return 0;
-		
-		double maior = carrinho.getItens().get(0).getValorTotal();
+		double maior = 0;
 		
 		for(Item item : carrinho.getItens()) {
 			if(item.getValorTotal() > maior) {
